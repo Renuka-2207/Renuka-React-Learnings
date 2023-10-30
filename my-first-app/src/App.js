b19e03a4e0eb4b7fed9919ed0138985f41efc8fa
2207renuka <2207renuka@gmail.com>
2023-10-30 12:37:17 +0530

    Made changes

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MyButton from './Components/Assignment1/MyButton';
 
 function App() {
 	return (
 		<div>
-<h1> Hello World </h1>
+<MyButton/>
     </div>
   );
 }
--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
-import MyButton from './Components/Assignment1/MyButton';
+
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<MyButton />);
+root.render(<App/>);
