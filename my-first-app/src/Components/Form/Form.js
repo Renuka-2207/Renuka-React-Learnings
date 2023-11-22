1e0be859d1b5034e7984e90a0bae77344372a865
2207renuka <2207renuka@gmail.com>
2023-11-22 11:19:33 +0530

    Error in form code

--- my-first-app/src/Components/Form/Form.js
+++ my-first-app/src/Components/Form/Form.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './Input.js';
 
 const Form = () => {
+  const [inputValues, setInputValues] =useState( {
+  fname: "",
+    lname: "",
+
+});
+  const [errors, setErrors] = useState( {} );
+ const [submitting, setSubmitting] = useState(false);
+  //Function to validate
+  const validateValues = (formData) => {
+    let errors = {};
+    if (formData.fname.length < 1) {
+      {errors.fname = "First Name field is required";
+    }
+    if (formData.lname.length < 1) {
+      errors.lname = "Last name field is required" ;
+    }
+    return errors;
+  };
+  //on change event handler
+  const handleChange = (e) => {
+    setInputValues( {
+      ...inputValues, [e.target.name] :e.target.value 
+    });
+  };
+  //submit function
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validateValues(inputValues));
+    setSubmitting(true);
+  };
+
   return (
-    <form>
+    <form onSubmit = {handleSubmit}>
             {/* Use the custom Input component for the all the form fields */}
-      <Input label="First Name" id="fname" type="text" placeholder="Enter your first name" autoComplete="given-name"/>
+      <Input label="First Name" id="fname" type="text" placeholder="Enter your first name" autoComplete="given-name" value = {inputValues.fname} onChange = {handleChange}/>
       <br/><br/>
 
-      <Input label="Last Name" id="lname" type="text" placeholder="Enter your last name" autoComplete="family-name"/>
+      <Input label="Last Name" id="lname" type="text" placeholder="Enter your last name" autoComplete="family-name" value = {inputValues.lname} onChange = {handleChange}/>
       <br/><br/>
 
       <Input label="Contact Number" id="phone" type="tel" placeholder="Enter your mobile number" autoComplete="tel"/>
@@ -24,5 +55,6 @@ const Form = () => {
     </form>
   );
 };
+
 
 export default Form;
--- my-first-app/src/Components/Form/Input.js
+++ my-first-app/src/Components/Form/Input.js
@@ -1,15 +1,14 @@
-// Import the React library to define React components
 import React from 'react';
 
 // Define a functional component named Input that takes in props: label, id, type, placeholder, and autoComplete
-const Input = ({ label, id, type, placeholder, autoComplete }) => {
+const Input = ({ label, id, type, placeholder, autoComplete, value, onChange }) => {
   // Render a div containing a label and an input element
   return (
     <div>
       {/* Associate the label with the input using the 'for' attribute */}
       <label htmlFor={id}>{label}</label>
       {/* Render the input element with specified id, type, placeholder, and autoComplete attributes */}
-      <input id={id} type={type} placeholder={placeholder} autoComplete={autoComplete} />
+      <input id={id} type={type} placeholder={placeholder} autoComplete={autoComplete} value={value} onChange={onChange} />
     </div>
   );
 };
